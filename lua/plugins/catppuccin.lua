3e9fac1aa9c28e62ff6cb7002635876f8795c252
Shiwei Ge <geshiwei@google.com>
2026-03-13 21:15:14 +0000

    first commit

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "macchiato", -- latte, frappe, macchiato, mocha
+      transparent_background = true,
+      integrations = {
+        telescope = true,
+        mason = true,
+        nvimtree = true,
+        notify = true,
+        mini = true,
+      },
+    },
+  },
+}
